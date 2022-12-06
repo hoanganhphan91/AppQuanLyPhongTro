f6258da3d64054c49c93cdcf7aec62a72074f8dc
phan <https://github.com/xuyen97bg/DuAn1>
2022-12-06 07:59:07 +0700

    J

--- app/src/main/java/com/example/duan1/LoginActivity.java
+++ app/src/main/java/com/example/duan1/LoginActivity.java
@@ -44,11 +44,8 @@ public class LoginActivity extends AppCompatActivity {
                 }else {
                     SessionManage sessionManage = new SessionManage(this);
                     sessionManage.saveAccount(account);
-<<<<<<< HEAD
-                    Intent intent = new Intent(this,RoomManageActivity.class);
-=======
+
                     Intent intent = new Intent(this,MenuMainActivity.class);
->>>>>>> origin/master
                     startActivity(intent);
                 }
             }else {
